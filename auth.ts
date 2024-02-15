69d3df53e07e1395062b983b3eee21d1ef3d1ba3
Vijaysj <vijaysangwan105@gmail.com>
2024-02-15 15:45:02 +0530

    check

--- auth.ts
+++ auth.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth"
 import { UserRole } from "@prisma/client";
 import { PrismaAdapter } from "@auth/prisma-adapter";
+
 import { db } from "@/lib/db";
 import authConfig from "@/auth.config";
 import { getUserById } from "@/data/user";
